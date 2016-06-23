55dfc55c3c428babf4ef9749aa1d72d4580d46e8
David Baumann <david_baumann@student.uml.edu>
2016-06-23 13:21:53 -0400

    added error checking

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,21 @@
 
 int main(int argc, char* argv[]) {
   TemperatureSensor tmp102(1, 0x48, 0x00, 0x00, 0x00);
-  tmp102.initDevice();
-  tmp102.initTempSensor();
-  std::cout << tmp102.readTemp() << std::endl;
+  if (tmp102.initDevice() == false) {
+    cerr << "Error: I2C bus failed to open." << endl;
+    return false;
+  }
+  cout << "Opened I2C bus" << endl;
+  if (tmp102.initTempSensor() == false) {
+    cerr << "Error: Temperature Sensor failed to initalize." << endl;
+    return false;
+  }
+  cout << "Initialized Temperature Sensor" << endl;
+  cout << "Reading Temperature data..." << endl;
+  for (int i = 0; i < 5; i++) {
+    cout << "Temperature = " << (int) tempSensor.readTemp() << endl;
+    usleep(500000);
+  }
+  return true;
   return 0;
 }
